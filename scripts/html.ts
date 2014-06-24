78e571125d19484c54c97c3927a2c19eed7f345d
Steve Minor <steve.e.minor@nordstrom.com>
2014-06-24 14:55:11 -0700

    the nightmare begins

---
+++ scripts/html.ts
@@ -0,0 +1,19 @@
+# HTML Transformations go here
+
+$("/html") {
+  add_canonical_tag()
+  rewrite_links()
+  absolutize()
+  rewrite_aspnet_scripts()
+  clean_mobile_meta_tags()
+  remove_all_styles()
+
+  add_assets()
+
+
+  @import "sections/header.ts"
+  @import "sections/footer.ts"
+
+  @import "mappings.ts"
+
+}
